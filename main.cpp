ce7c17ffab1872ac4e82750bc6b34338964305b7
Frank Zimmermann <fz@zenmeister.de>
2020-02-07 17:03:17 +0100

    GUI für HiPDI

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_WIN
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor);
+
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
